16b60eaf9957e52e5931b0e5374572aff0477bfe
Erik Watson <championchap@gmail.com>
2021-02-28 15:31:38 +0000

    removing the visual flash on startup

--- src/main/main.js
+++ src/main/main.js
@@ -2,6 +2,14 @@ const { app, BrowserWindow, Tray, Menu } = require('electron')
 const fs = require('fs')
 
 let tray;
+const trayMenuTemplate = [
+  { label: 'Add Filter', type: 'normal' },
+  { label: 'Edit Filters', type: 'normal' },
+  { type: 'separator' },
+  { label: 'Preferences', type: 'normal' },
+  { type: 'separator' },
+  { label: 'Close', type: 'normal', click: shutdown }
+]
 
 function shutdown () {
   app.exit(0)
@@ -13,26 +21,27 @@ function createFilterWindow () {
     height: 600,
     webPreferences: {
       nodeIntegration: true
-    }
+    },
+    show: false
   })
 
   win.loadFile('./dist/index.html')
+
+  win.once('ready-to-show', () => {
+    win.show()
+  })
 }
 
 function createNotificationMenu () {
   tray = new Tray('./dist/icon.png')
-
-  const contextMenu = Menu.buildFromTemplate([
-    { label: 'Add Filter', type: 'normal' },
-    { label: 'Edit Filters', type: 'normal' },
-    { type: 'separator' },
-    { label: 'Preferences', type: 'normal' },
-    { type: 'separator' },
-    { label: 'Close', type: 'normal', click: shutdown }
-  ])
-
   tray.setToolTip('Folder Filter')
+
+  const contextMenu = Menu.buildFromTemplate(trayMenuTemplate)
   tray.setContextMenu(contextMenu)
+}
+
+function destroyNotificationMenu () {
+  tray.destroy()
 }
 
 function start () {
